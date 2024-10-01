f17a89f96295b844e9f980b953c72bdabb12a8ff
Soumya <racrac786po@gmail.com>
2024-10-01 19:51:26 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import dotenv from "dotenv";
+import messageRouter from "./router/messageRouter.js";
+import cors from "cors";
+
+const app = express();
+
+dotenv.config({ path: "./config/config.env" });
+
+app.use(
+  cors()
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/message", messageRouter);
+
+dbConnection();
+
+export default app;
